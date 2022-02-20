bcc887fcd36b41674895986a8401de02551c29dc
pmaitland <pmaitland98@gmail.com>
2022-02-20 23:03:57 +0000

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import face from './face.jpg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <img src={face} className="header-logo" alt="face" />
+        <div className="header-title">
+          <div className="text">peter@maitland:~$</div>
+          <div className="text flash">|</div>
+        </div>
+      </div>
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
