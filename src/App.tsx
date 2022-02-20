2be7f3a5eb26219715de650330b9d668d95f698a
pmaitland <pmaitland98@gmail.com>
2022-02-20 23:16:37 +0000

    Created Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import face from './face.jpg'
 import './App.css'
+
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="app">
-      <div className="header">
-        <img src={face} className="header-logo" alt="face" />
-        <div className="header-title">
-          <div className="text">peter@maitland:~$</div>
-          <div className="text flash">|</div>
-        </div>
-      </div>
-      <div></div>
+      <Header />
     </div>
   )
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import face from '../../face.jpg'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={face} className="logo" alt="face" />
+      <div className="title">
+        <div className="text">peter@maitland:~$</div>
+        <div className="text flash">|</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
